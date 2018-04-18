1976ca4ef77b11b43d0f9d03ec198e9de78cecc6
romeorubiko <raulescabia255@gmail.com>
2018-04-18 19:44:42 +0200

    arreglado parametro con cast

--- src/ch/idsia/agents/controllers/Kaathe.java
+++ src/ch/idsia/agents/controllers/Kaathe.java
@@ -89,7 +89,7 @@ public class Kaathe extends BasicMarioAIAgent implements Agent {
 		
 		int situacion = funcion.pertenecia(ins);
 
-	   resultado = funcion.similitud(ins, pertenece[situacion]);
+	   resultado = funcion.similitud(ins, (Instancia[])funcion.pertenece[situacion].toArray());
 	   
 	   action[Mario.KEY_DOWN] = resultado.abajo;
        action[Mario.KEY_UP] = resultado.salto;
